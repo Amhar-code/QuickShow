cf250f403e8ccc2005d051ad3442de8030ac66b6
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-10-12 23:41:36 +0530

    Developed the Navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
-import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer.jsx'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Movies from './pages/Movies.jsx'
 import MovieDetails from './pages/MovieDetails.jsx'
 import SeatLayout from './pages/SeatLayouts.jsx'
 import MyBookings from './pages/MyBookings.jsx'
 import Favourite from './pages/Favourite.jsx'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
-      <Navbar />
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/movies' element={<Movies />} />
@@ -20,6 +25,7 @@ const App = () => {
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favourite' element={<Favourite />} />
       </Routes>
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
