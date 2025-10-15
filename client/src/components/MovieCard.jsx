4ada40006cf3e6e4085e9b969768cf21287ca8ee
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-10-15 23:51:32 +0530

    Added a Blur Circle component and enhance the Hero Section

---
+++ client/src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div className='flex flex-col justify-between p-3 bg-gray-800
+    rounded-2xl hover:-translate-y-1 transition duration-300 w-66'>
+         <img src={""} alt='' />
+    </div>
+  )
+}
+
+export default MovieCard
