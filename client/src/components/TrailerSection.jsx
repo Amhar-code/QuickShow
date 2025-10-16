12ff1b501a05f29880dccba76163a89dd3124ad8
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-10-16 11:17:15 +0530

    Enhanced the MovieCard Section

---
+++ client/src/components/TrailerSection.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { dummyTrailers } from '../assets/assets'
+
+const TrailerSection = () => {
+    const [ currentTrailer, setcrrentTrailer ] = useState(dummyTrailers[0]
+
+    )
+  return (
+    <div className='px-6 md:px-16 lg:pc-24'>
+
+    </div>
+  )
+}
+
+export default TrailerSection
