8f98eb751eaf125e3ece8a7e4c54fb3fd2c527d3
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-11-01 23:41:32 +0530

    Created the required files to admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,11 @@ import SeatLayout from './pages/SeatLayouts.jsx'
 import MyBookings from './pages/MyBookings.jsx'
 import Favourite from './pages/Favourite.jsx'
 import { Toaster } from 'react-hot-toast'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import ListShows from './pages/admin/ListShows.jsx'
+import ListBookings from './pages/admin/ListBookings.jsx'
+import Layout from './pages/admin/Layout.jsx'
+import { AddShows } from './pages/admin/AddShows.jsx'
 
 const App = () => {
 
@@ -24,6 +29,12 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout />} />
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favourite' element={<Favourite />} />
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-shows' element={<AddShows />} />
+          <Route path='lsit-shows' element={<ListShows />} />
+          <Route path='list-bookings' element={<ListBookings />} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
