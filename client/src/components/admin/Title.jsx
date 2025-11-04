67fd9994b737a24800a77013a55fb471f10c83b2
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-11-04 18:34:05 +0530

    Set up initial version of the admin dashboard

--- client/src/components/admin/Title.jsx
+++ client/src/components/admin/Title.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Title = ({ text1, text2 }) => {
     return (
-        <h1 className='font-medium text-2xl'>
-            {text1}<span className='underline text-primary'>
+        <h1 className="font-medium text-2xl">
+            {text1}
+            <span className="underline text-primary ml-1">
                 {text2}
             </span>
         </h1>
