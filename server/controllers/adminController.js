02d3ee29e64055e4d0bbc46d665ca1c4837a74ad
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-11-20 23:31:44 +0530

    feat: implement admin dashboard endpoints and integrate controller, router and server updates

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -29,3 +29,28 @@ export const getDashboardData = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+//API to get all shows
+export const getAllShows = async (req, res) => {
+    try {
+        const shows = await Show.find({ showDateTime: { $gte: new Date() } }).populate('movie').sort({ showDateTime: 1 });
+        res.json({ success: true, shows })
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+//API to get all bookings
+export const getAllBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find({}).populate('user').populate({
+            _path: 'shows',
+            populate: { path: 'movie' }
+        }).sort({ createdAt: -1 });
+        res.json({ success: true, bookings })
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+}
