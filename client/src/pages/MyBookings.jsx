300dda9a01f431dc31a6397d72c8904c1aaeacef
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-11-27 11:14:52 +0530

    Fixed a bug

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -33,7 +33,7 @@ const MyBookings = () => {
   }, [user])
 
   return !isloading ? (
-    <div className='relative px-6 md:px-16 lg-px-40 pt-30 md:pt-40 
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 
     min-h-[80vh]'>
       <BlurCircle top='100px' left='100px' />
       <div>
@@ -43,13 +43,16 @@ const MyBookings = () => {
       {bookings.map((item, index) => (
         <div key={index} className='flex flex-col md:flex-row justify-between
         bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'>
-          <div className='flex flex-col md:flex-row'>
-            <img src={image_base_url + item.show.movie.poster_path} alt='' className='md:max-w-45
-            aspect-video h-auto object-cover object-bottom rounded' />
-            <div className='flex flex-col p-4'>
+          <div className='flex flex-col md:flex-row gap-4'>
+            <img
+              src={`${image_base_url}/${item.show.movie.poster_path}`}
+              alt={item.show.movie.title}
+              className='md:max-w-45 w-full md:w-auto aspect-video h-auto object-cover object-center rounded'
+            />
+            <div className='flex flex-col justify-center gap-1'>
               <p className='text-lg font-semibold'>{item.show.movie.title}</p>
               <p className='text-gray-400 text-sm'>{timeFormat(item.show.movie.runtime)}</p>
-              <p className='text-gray-400 text-sm mt-auto'>{dateFormat(item.show.showDateTime)}</p>
+              <p className='text-gray-400 text-sm'>{dateFormat(item.show.showDateTime)}</p>
             </div>
           </div>
           <div className='flex flex-col md:items-end md:text-right
@@ -57,12 +60,12 @@ const MyBookings = () => {
             <div className='flex items-center gap-4'>
               <p className='text-2xl font-semibold mb-3'>{currency}{item.amount}</p>
               {!item.isPaid && <button className='bg-primary px-4 py-1.5 mb-3
-              text-sm rounded-full font medium cursor-pointer'>Pay Now</button>}
+              text-sm rounded-full font-medium cursor-pointer'>Pay Now</button>}
             </div>
             <div className='text-sm'>
-              <p><span className='text-gray-400'>Total Tickets:</span>
+              <p><span className='text-gray-400'>Total Tickets: </span>
                 {item.bookedSeats.length}</p>
-              <p><span className='text-gray-400'>Seat Numbers:</span>
+              <p><span className='text-gray-400'>Seat Numbers: </span>
                 {item.bookedSeats.join(", ")}</p>
             </div>
           </div>
