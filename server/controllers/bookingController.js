d13971a7a50f18d688a1a09678510c7c43a9c50c
M.I.Amhar <164432283+Amhar-code@users.noreply.github.com>
2025-12-01 21:57:22 +0530

    Updated by backedend with payment gateway

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -50,9 +50,32 @@ export const createBooking = async (req, res) => {
 
         //payment gateway
         const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
-        
 
-        res.json({ success: true, message: "Booked Successfully" });
+        const line_items = [{
+            price_data: {
+                currency: 'usd',
+                product_name: {
+                    name: showData.movie.title
+                },
+                unit_amount: Math.floor(booking.amount) * 100
+            },
+            quantity: 1
+        }];
+
+        const session = await stripeInstance.checkout.sessions.create({
+            success_url: `${origin}/loading/my-bookings`,
+            cancel_url: `${origin}/my-bookings`,
+            line_items: line_items,
+            mode: 'payments',
+            metadata: {
+                bookingId: booking._id.toString()
+            },
+            expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
+        })
+
+        booking.paymentLink = session.url;
+        await booking.save();
+        res.json({ success: true, url: session.url });
     } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
